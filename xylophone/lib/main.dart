00fbb7aa9909e92a899fb84b532c8f3404f3f95c
ilakya2905 <58387111+ilakya2905@users.noreply.github.com>
2021-02-08 15:48:07 +0530

    Add files via upload

---
+++ xylophone/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber){
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+  Expanded buildKey({int soundNumber, Color colorName}){
+    return Expanded(
+      child: FlatButton(
+
+        color: colorName,
+        onPressed: (){
+          playSound(soundNumber);
+        },
+
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(soundNumber: 1, colorName: Colors.red),
+              buildKey(soundNumber: 2, colorName: Colors.orange),
+              buildKey(soundNumber: 3, colorName: Colors.yellow),
+              buildKey(soundNumber: 4, colorName: Colors.green),
+              buildKey(soundNumber: 5, colorName: Colors.teal),
+              buildKey(soundNumber: 6, colorName: Colors.blue),
+              buildKey(soundNumber: 7, colorName: Colors.purple),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
